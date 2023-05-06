ac5ee41fb429d7fa804c05fd4c62bb238c0d4455
Kris Hamade <khamade22@gmail.com>
2023-05-06 00:47:20 -0400

    Merge remote-tracking branch 'origin/feature/#6-gpt-4-integration' into feature/#6-gpt-4-integration

--- src/openai/gpt.js
+++ src/openai/gpt.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const {
+    characterLimit
+  } = require('../utils/data-misc/config.js');
+const {
     Configuration,
     OpenAIApi
 } = require("openai");
+const {
+    getHistory
+} = require('../discord/historyLog.js');
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -12,16 +18,23 @@ const openai = new OpenAIApi(configuration);
 
 const haggleStatsFilePath = path.join('.', 'src', 'utils', 'data-misc', 'haggle-stats.json');
 
-async function generateResponse(prompt, persona, dndData) {
+// Set the max prompt size * 4 is about to calculate token size
+// characterLimit is set in the config.js file
+const maxPromptSize = characterLimit;
+
+async function generateResponse(prompt, persona, dndData, nickname) {
     // Read in the file containing the haggle stats
     let haggleStats = JSON.parse(fs.readFileSync(`${haggleStatsFilePath}`, 'utf8'));
 
     haggleStatsPrompt = `You have died **${haggleStats.haggleDeaths} times** and Valon has had to spend **${haggleStats.moneySpent} GOLD** getting him back.`
 
+    const chatHistory = await getSizedHistory(prompt, persona, haggleStatsPrompt, dndData);
+
     console.log("Generating response for prompt:", prompt); // Log the prompt
     console.log("Using persona:", persona); // Log the persona
     console.log("Using Haggle Stats:", haggleStatsPrompt); // Log the Haggle Stats (if any)
     console.log("Using D&D Data:", dndData); // Log the D&D Data (if any)
+    console.log("Using History:", chatHistory); // Log the History (if any)
 
     try {
         const response = await openai.createChatCompletion({
@@ -39,12 +52,15 @@ async function generateResponse(prompt, persona, dndData) {
                     content: "--START DUNGEONS AND DRAGONS CAMPAIGN DATA-- " + dndData + " --END DUNGEONS AND DRAGONS CAMPAIGN DATA--"
                 },
                 {
+                    role: "system",
+                    content: "Chat History will follow this. It is in JSON format. type: is either the user or assistant. username is who sent it. content is the message. timestamp is when the message was sent " + "--START CHAT HISTORY-- " + chatHistory + " --END CHAT HISTORY--"
+                },
+                {
                     role: "user",
-                    content: prompt
+                    content: `${nickname} says: ${prompt}`
                 }
             ]
         });
-
 
         const message = response.data.choices[0].message.content;
 
@@ -64,6 +80,20 @@ async function generateResponse(prompt, persona, dndData) {
         return errorMessage; // Return an empty string if an error occurs
     }
 }
+
+async function getSizedHistory(prompt, persona, haggleStatsPrompt, dndData) {
+    const promptLength = prompt.length;
+    const personaLength = JSON.stringify(persona).length;
+    const haggleStatsPromptLength = haggleStatsPrompt.length;
+    const dndDataLength = JSON.stringify(dndData).length;
+
+    const totalLength = promptLength + personaLength + haggleStatsPromptLength + dndDataLength;
+    const remainingSize = maxPromptSize - totalLength;
+
+    const historyItems = await getHistory(remainingSize);
+    return historyItems;
+}
+
 
 module.exports = {
     generateResponse
