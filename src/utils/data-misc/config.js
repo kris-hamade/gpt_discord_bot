bf5df8e33a5eb001ac9bd0a82e0067a2c6747b90
Kris Hamade <khamade22@gmail.com>
2023-05-11 13:33:03 -0400

    Modified gpt.js and config.js to fix temperature

--- src/utils/data-misc/config.js
+++ src/utils/data-misc/config.js
@@ -2,12 +2,12 @@
 // Instantiate moment.js for timestamping
 const moment = require('moment');
 // Sets the version of the bot
-const version = "1.4.1.0";
+const version = "1.4.1.1";
 // Sets the GPT model
-let gptModel = "gpt-4";
+let gptModel = "gpt-3.5-turbo";
 // Sets the GPT temperature
 // Causing an issue when used in gpt.js
-let gptTemperature = "1";
+let gptTemperature = 1;
 // Sets Character limit in gpt.js and preprocessor.js
 let characterLimit = 8000 * 4;
 
@@ -15,10 +15,10 @@ let characterLimit = 8000 * 4;
 const startTime = moment();
 
 function setGptModel(newModel) {
-    const allowedModels = ["gpt-3","gpt-3.5-turbo", "gpt-4"];
+    const allowedModels = ["gpt-3", "gpt-3.5-turbo", "gpt-4"];
     if (allowedModels.includes(newModel)) {
         // Ternary operator to set gptModel to gpt-3.5-turbo if gpt-3 is selected
-        gptModel = newModel === "gpt-3" ? "gpt-3.5-turbo" : newModel;        
+        gptModel = newModel === "gpt-3" ? "gpt-3.5-turbo" : newModel;
 
         characterLimit = newModel === "gpt-4" ? 8000 * 4 : 4000 * 4;
         return {
@@ -36,10 +36,22 @@ function setGptModel(newModel) {
 }
 
 function setGptTemperature(newTemperature) {
-    if (newTemperature >= 0 && newTemperature <= 2) {
-        gptTemperature = newTemperature;
+    // Convert the input to a number in case it's a string
+    const temperature = parseFloat(newTemperature);
+
+    if (!isNaN(temperature) && temperature >= 0 && temperature <= 1) {
+        gptTemperature = temperature;
+        return {
+            success: true,
+            message: `Set GPT temperature to ${newTemperature}.`
+        };
     } else {
-        console.error("Invalid GPT temperature. Temperature should be between 0 and 2.");
+        const errorMessage = `Invalid GPT temperature: ${newTemperature}. Temperature should be between 0 and 1.`;
+        console.error(errorMessage);
+        return {
+            success: false,
+            message: errorMessage
+        };
     }
 }
 
@@ -49,6 +61,10 @@ function getCharacterLimit() {
 
 function getGptModel() {
     return gptModel;
+}
+
+function getGptTemperature() {
+    return gptTemperature;
 }
 
 function getUptime() {
@@ -68,10 +84,10 @@ function getConfigInformation() {
 
 module.exports = {
     version,
-    gptTemperature,
     getCharacterLimit,
     getConfigInformation,
     getGptModel,
+    getGptTemperature,
     getUptime,
     setGptModel,
     setGptTemperature,
