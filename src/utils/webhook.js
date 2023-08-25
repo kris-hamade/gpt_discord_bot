8f7e40adec32a9c6e17dbdab384e642fd1de8ff0
Kris Hamade <kris.hamade@hamy.dev>
2023-08-25 17:29:30 -0400

    Added Summarized Report for Vulns

--- src/utils/webhook.js
+++ src/utils/webhook.js
@@ -1,5 +1,6 @@
 const WebhookSubs = require('../models/webhookSub');
 const client = require('../discord/client');
+const { generateVulnerabilityReport } = require('../openai/gpt')
 
 let subs = [];
 
@@ -29,29 +30,37 @@ async function processWebhook(data) {
                     messageContent = `${data.event}\n${data.subject}\n${data.image}`;
                     await pingChannel(channelId, messageContent);
                     break;
-                    case "cve-aggregator":
-                        let messageContents = [];
-                        for (const cve of data.content.vulnerabilities) {
-                            const singleMessageContent = 
-                    `\`\`\`CVE ID: ${cve.CVE_ID}\n Status: ${cve.Status} \n Published Date: ${cve.Published_Date} \n Last Modified Date: ${cve.Last_Modified_Date} \n Description: ${cve.Description} \n CVSS v3 Score: ${cve.CVSS_v3_Score} \n CVSS v3 Severity: ${cve.CVSS_v3_Severity} \n CVSS v2 Score: ${cve.CVSS_v2_Score} \`\`\``;
-                    
-                            messageContents.push(singleMessageContent);
-                        }
-                    
-                        // Chunk messages if needed (Discord's limit is 2000 chars)
-                        let chunkedMessage = '';
-                        for (const content of messageContents) {
-                            if ((chunkedMessage.length + content.length) > 1900) { // give some buffer
-                                await pingChannel(channelId, chunkedMessage);
-                                chunkedMessage = '';
-                            }
-                            chunkedMessage += content + '\n';
-                        }
-                        
-                        if (chunkedMessage) {
+                case "cve-aggregator":
+                    let messageContents = [];
+                    for (const cve of data.content.vulnerabilities) {
+                        const singleMessageContent =
+                            `\`\`\`CVE ID: ${cve.CVE_ID}\n Status: ${cve.Status} \n Published Date: ${cve.Published_Date} \n Last Modified Date: ${cve.Last_Modified_Date} \n Description: ${cve.Description} \n CVSS v3 Score: ${cve.CVSS_v3_Score} \n CVSS v3 Severity: ${cve.CVSS_v3_Severity} \n CVSS v2 Score: ${cve.CVSS_v2_Score} \`\`\``;
+
+                        messageContents.push(singleMessageContent);
+                    }
+
+                    // Chunk messages if needed (Discord's limit is 2000 chars)
+                    let chunkedMessage = '';
+                    for (const content of messageContents) {
+                        if ((chunkedMessage.length + content.length) > 1900) { // give some buffer
                             await pingChannel(channelId, chunkedMessage);
+                            chunkedMessage = '';
                         }
-                        break;
+                        chunkedMessage += content + '\n';
+                    }
+
+                    if (chunkedMessage) {
+                        await pingChannel(channelId, chunkedMessage);
+                    }
+                    break;
+                case "cve-full-report":
+                    const allCVEs = data.content.vulnerabilities.map(cve => {
+                        return `CVE ID: ${cve.CVE_ID}\nStatus: ${cve.Status}\nPublished Date: ${cve.Published_Date}\nLast Modified Date: ${cve.Last_Modified_Date}\nDescription: ${cve.Description}\nCVSS v3 Score: ${cve.CVSS_v3_Score}\nCVSS v3 Severity: ${cve.CVSS_v3_Severity}\nCVSS v2 Score: ${cve.CVSS_v2_Score}`;
+                    }).join('\n\n');
+
+                    const report = await generateVulnerabilityReport(allCVEs);
+                    console.log("THIS IS THE REPORT, ", report)
+                    await pingChannel(channelId, report)
             }
         }
     } else {
