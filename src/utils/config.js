67619f35f2a7090eaae5ba4922baf967f2e5ed42
Kris Hamade <kris.hamade@hamy.dev>
2024-05-14 13:20:26 +0000

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 // Instantiate moment.js for timestamping
 const moment = require('moment');
 // Sets the version of the bot
-const version = "1.7.3";
+const version = "1.7.3.0";
 // Sets Character limit in gpt.js and preprocessor.js
 let characterLimit = 96000 * 4;
 
